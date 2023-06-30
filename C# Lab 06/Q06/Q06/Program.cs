2d819ee04915f1a101faf37ce08109f031f15db3
binuthi <135793460+binuthi@users.noreply.github.com>
2023-06-30 11:05:51 +0530

    Add files via upload

---
+++ C# Lab 06/Q06/Q06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array:");
+            int size = int.Parse(Console.ReadLine());
+
+            @class objclass = new @class();
+            objclass.array(size);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
