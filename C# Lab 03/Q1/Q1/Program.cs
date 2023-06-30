ee6a230fb76465e0222728f3808b1131ed9631e2
binuthi <135793460+binuthi@users.noreply.github.com>
2023-06-30 11:19:02 +0530

    Add files via upload

---
+++ C# Lab 03/Q1/Q1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a numer: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Lab 03/Q2/Q2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    { 
+        public class Pro
+        {
+            public static void Main(string[] args)
+            {
+                Console.Write("Enter a string: ");
+                string input = Console.ReadLine();
+
+                int vowelCount = CountVowels(input);
+
+                Console.WriteLine("Number of vowels: " + vowelCount);
+            }
+
+            public static int CountVowels(string str)
+            {
+                int count = 0;
+
+               
+                string lowerCaseStr = str.ToLower();
+
+                
+                foreach (char c in lowerCaseStr)
+                {
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+    }
+}
+}
