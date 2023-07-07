9bb384a64779e45b076db2209e968ac51fe69993
binuthi <135793460+binuthi@users.noreply.github.com>
2023-07-07 16:23:23 +0530

    Add files via upload

---
+++ C# Lab 06/Q08/lab06Q08/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06Q08
+{
+    internal class Animal
+    {
+            public void DisplayAnimalInfo()
+            {
+                Console.WriteLine("I am an animal");
+            }
+        
+    }
+}
---
+++ C# Lab 06/Q08/lab06Q08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab06Q08
+{
+    internal class Program
+    {
+        public class Pro
+        {
+            public static void Main(string[] args)
+            {
+                Animal animal = new Animal();
+                dog dog = new dog();
+
+                animal.DisplayAnimalInfo(); 
+                dog.Display();
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
+
